172b3b8c25af47f0f9f6b931d144f67ff29fba30
Anusha-Yadavv <anushakottam22@gmail.com>
2024-02-10 18:57:25 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 
 import React from "react";
 import './../styles/App.css';
-import Counter from './Components/Counter';
+import Counter from './components/Counter';
 
 const App = () => {
   return (
