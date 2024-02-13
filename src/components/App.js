2795f52f917e56e0bd84107c88cce01879d0f453
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2024-02-13 22:21:40 +0530

    correcting logic

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
 
 import React from "react";
 import './../styles/App.css';
+<<<<<<< HEAD
+import { useState } from "react"
+=======
 import Counter from './components/Counter';
+>>>>>>> e102f489d96b7a3833839346becc396d0b8c7051
+
 
 const App = () => {
+  const [count,setCount]=useState(0);
+  const clickMe=()=>{
+    setCount(count+1);
+  }
+  
   return (
     <div>
         {/* Do not remove the main div */}
-        <Counter/>
+<<<<<<< HEAD
+        <p>Button clicked {count} times</p>
+        <button onClick={clickMe}>Click me</button>
+      
+=======
+>>>>>>> e102f489d96b7a3833839346becc396d0b8c7051
     </div>
   )
 }
